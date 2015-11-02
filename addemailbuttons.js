f5e7dbda4f201aab604a7ff925bdb28b6fb35d8b
David Poeschl <dpoeschl@gmail.com>
2015-11-01 20:48:23 -0800

    Distinguish between Issues & Pull Requests
    Fixes #8

--- addemailbuttons.js
+++ addemailbuttons.js
@@ -34,28 +34,29 @@ function reload() {
 }
 
 function addIndividualIssueButton() {
-    chrome.runtime.sendMessage({ method: "getSettings", keys: ["emailIssue"] }, function (response) {
-        if (response.data["emailIssue"]) {
-            var titleElement = document.getElementsByClassName("js-issue-title")[0]
+    var urlParts = stripTrailingSlash(window.location.href.substr(0, window.location.href.indexOf('#'))).split("/");
+    var isPull = urlParts[urlParts.length - 2] == "pull";
 
-            if (typeof titleElement !== 'undefined') {
-                var button = document.createElement("input");
-                button.setAttribute("type", "button");
-                button.setAttribute("value", "Email Issue");
-                button.setAttribute("name", "buttonname");
-                var issueNumber = document.getElementsByClassName("gh-header-number")[0].innerHTML.substring(1);
-                var issueTitle = document.getElementsByClassName("js-issue-title")[0].innerHTML;
-                button.onclick = (function () {
-                    var currentTitle = issueTitle;
-                    var currentNumber = issueNumber;
-                    return function () {
-                        sendmail(currentNumber, currentTitle);
-                    };
-                });
+    chrome.runtime.sendMessage({ method: "getSettings", keys: ["emailIssue", "emailPullRequest"] }, function (response) {
+        var titleElement = document.getElementsByClassName("js-issue-title")[0]
+        if (typeof titleElement !== 'undefined' && ((isPull && response.data["emailPullRequest"]) || (!isPull && response.data["emailIssue"]))) {
+            var button = document.createElement("input");
+            button.setAttribute("type", "button");
+            button.setAttribute("value", isPull ? "Email PR" : "Email Issue");
+            button.setAttribute("name", "buttonname");
+            var issueNumber = document.getElementsByClassName("gh-header-number")[0].innerHTML.substring(1);
+            var issueTitle = document.getElementsByClassName("js-issue-title")[0].innerHTML;
+            button.onclick = (function () {
+                var currentTitle = issueTitle;
+                var currentNumber = issueNumber;
+                var currentIsPull = isPull;
+                return function () {
+                    sendmail(currentNumber, currentTitle, currentIsPull);
+                };
+            })();
 
-                button.className = "btn " + emailGitHubIssuesClassName;
-                titleElement.parentNode.insertBefore(button, document.getElementsByClassName("js-issue-title")[0].parentNode.firstChild);
-            }
+            button.className = "btn " + emailGitHubIssuesClassName;
+            titleElement.parentNode.insertBefore(button, document.getElementsByClassName("js-issue-title")[0].parentNode.firstChild);
         }
     });
 }
@@ -324,38 +325,49 @@ function makeBuildStatusWindowsBig() {
     }
 }
 
+function stripTrailingSlash(str) {
+    if(str.substr(-1) === '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
+}
+
 function addButtonsToIssuesList() {
-    chrome.runtime.sendMessage({ method: "getSettings", keys: ["emailIssuesList"] }, function (response) {
-        if (response.data["emailIssuesList"]) {
-            var issuesList = document.getElementsByClassName("table-list-issues")[0];
+    var url = stripTrailingSlash(window.location.href);
+    var urlParts = url.split("/");
 
-            if (typeof issuesList !== 'undefined') {
-                var issues = issuesList.children;
+    var isPull = false;
+    if (urlParts[urlParts.length - 1] == "pulls") {
+      isPull = true;
+    }
 
-                for (var i = 0; i < issues.length; i++) {
-                    var issue = issues[i];
-                    var title = issue.getElementsByClassName("issue-title")[0];
+    chrome.runtime.sendMessage({ method: "getSettings", keys: ["emailIssuesList", "emailPullRequestList"] }, function (response) {
+        var issuesList = document.getElementsByClassName("table-list-issues")[0];
+        if (typeof issuesList !== 'undefined' && ((isPull && response.data["emailPullRequestList"]) || (!isPull && response.data["emailIssuesList"]))) {
+            var issues = issuesList.children;
 
-                    var button = document.createElement("input");
-                    button.setAttribute("type", "button");
-                    button.setAttribute("value", "Email Issue");
-                    button.setAttribute("name", "buttonname");
+            for (var i = 0; i < issues.length; i++) {
+                var issue = issues[i];
+                var title = issue.getElementsByClassName("issue-title")[0];
+                var button = document.createElement("input");
+                button.setAttribute("type", "button");
+                button.setAttribute("value", isPull ? "Email PR" : "Email Issue");
+                button.setAttribute("name", "buttonname");
 
-                    var urlParts = title.getElementsByClassName("issue-title-link")[0].href.split("/");
-                    var issueNumber = urlParts[urlParts.length - 1];
-                    var issueTitle = title.getElementsByClassName("issue-title-link")[0].innerHTML;
+                var urlParts = title.getElementsByClassName("issue-title-link")[0].href.split("/");
+                var issueNumber = urlParts[urlParts.length - 1];
+                var issueTitle = title.getElementsByClassName("issue-title-link")[0].innerHTML;
 
-                    button.onclick = (function () {
-                        var currentTitle = issueTitle;
-                        var currentNumber = issueNumber;
-                        return function () {
-                            sendmail(currentNumber, currentTitle);
-                        };
-                    })();
-                    button.className = "btn btn-sm " + emailGitHubIssuesClassName;
+                button.onclick = (function () {
+                    var currentTitle = issueTitle;
+                    var currentNumber = issueNumber;
+                    return function () {
+                        sendmail(currentNumber, currentTitle, isPull);
+                    };
+                })();
+                button.className = "btn btn-sm " + emailGitHubIssuesClassName;
 
-                    title.insertBefore(button, title.firstChild);
-                }
+                title.insertBefore(button, title.firstChild);
             }
         }
     });
@@ -373,16 +385,18 @@ function openJenkinsDetailsInNewTab() {
     });
 }
 
-function sendmail(issueNumber, issueTitle) {
+function sendmail(issueNumber, issueTitle, isPull) {
     issueTitle = issueTitle.trim();
     issueNumber = issueNumber.trim();
 
-    var baseUrl = document.getElementsByClassName("entry-title public")[0].getElementsByTagName('strong')[0].getElementsByTagName('a')[0].href + "/issues/";
+    var baseUrl = document.getElementsByClassName("entry-title public")[0].getElementsByTagName('strong')[0].getElementsByTagName('a')[0].href;
+    var kind = isPull ? "PR" : "Issue";
+    baseUrl = baseUrl + (isPull ? "/pull/" : "/issues/");
 
     var owner = document.getElementsByClassName("entry-title public")[0].getElementsByClassName("author")[0].getElementsByTagName("span")[0].innerHTML;
     var repo = document.getElementsByClassName("entry-title public")[0].getElementsByTagName("strong")[0].getElementsByTagName("a")[0].innerHTML;
 
-    var subject = owner + "/" + repo + " #" + issueNumber + ": " + issueTitle;
+    var subject = owner + "/" + repo + " " + kind + " #" + issueNumber + ": " + issueTitle;
 
     var body = baseUrl + issueNumber + "\r\n\r\n"; // TODO: Assigned to, etc.
 
