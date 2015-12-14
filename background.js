ee0f0dddb10877982b48d16c7f05736b01d57836
David Poeschl <dpoeschl@gmail.com>
2015-12-13 16:11:48 -0800

    Format background.js

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
-chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+chrome.runtime.onMessage.addListener(function (request, sender, callback) {
     if (request.action == "xhttp") {
         var xhttp = new XMLHttpRequest();
         var method = request.method ? request.method.toUpperCase() : 'GET';
 
-        xhttp.onload = function() {
+        xhttp.onload = function () {
             callback(xhttp.responseText);
         };
-        xhttp.onerror = function() {
+        xhttp.onerror = function () {
             callback();
         };
         xhttp.open(method, request.url, true);
@@ -17,15 +17,16 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
         return true; // prevents the callback from being called too early on return
     }
     else if (request.method == "getSettings") {
-      var keys = {};
-      for (var i = 0; i < request.keys.length; i++) {
-        keys[request.keys[i]] = true;
-      }
+        var keys = {};
 
-      chrome.storage.sync.get(keys, function(items) {
-        callback({data: items});
-      });
+        for (var i = 0; i < request.keys.length; i++) {
+            keys[request.keys[i]] = true;
+        }
 
-      return true;
+        chrome.storage.sync.get(keys, function (items) {
+            callback({ data: items });
+        });
+
+        return true;
     }
 });
